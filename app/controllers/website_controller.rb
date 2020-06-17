010dae8170caf5dd5c7668c166a6aa4a084ff7ca
jerryg <jgitsis@alignable.com>
2020-06-17 13:04:58 -0400

    menu

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -3,4 +3,13 @@ class WebsiteController < ApplicationController
   def index
   	
   end
+
+  def menu
+  	send_file(
+      "#{Rails.root}/public/coal_tower_menu.pdf",
+      filename: "coal_tower_menu.pdf",
+      type: "application/pdf",
+      disposition: 'inline'
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  root to: 'website#index'
+ get "/menu", to: 'website#menu'
 end
