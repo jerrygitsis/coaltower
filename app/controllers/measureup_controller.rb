7fa5a0891e45815f3662a3ed9d3e79d1b044224c
jerrygitsis <jerrygitsis@gmail.com>
2022-06-20 19:46:43 -0400

    measure up updates

--- app/controllers/measureup_controller.rb
+++ app/controllers/measureup_controller.rb
@@ -3,4 +3,26 @@ class MeasureupController < ApplicationController
     def index
         
     end
+
+    def aboutme
+        
+    end
+
+    def menu
+      send_file(
+        "#{Rails.root}/public/measureup_menu.pdf",
+        filename: "measureup_menu.pdf",
+        type: "application/pdf",
+        disposition: 'inline'
+      )
+    end
+
+    def macros
+      send_file(
+        "#{Rails.root}/public/measureup_macros.pdf",
+        filename: "measureup_macros.pdf",
+        type: "application/pdf",
+        disposition: 'inline'
+      )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  constraints(lambda { |request| request.original_url.include?(".measureup4u.") }) do
-    root to: 'measureup#index'
+  constraints(lambda { |request| request.original_url.include?("coaltower") }) do  
+    root to: 'website#index'
+    get "/menu", to: 'website#menu'
+    get "/breakfast", to: 'website#breakfast'
   end
-    
-  root to: 'website#index'
-  get "/menu", to: 'website#menu'
-  get "/breakfast", to: 'website#breakfast'
+  
+  constraints(lambda { |request| request.original_url.include?("measureup") }) do  
+    get "/home", to: 'measureup#index'
+    get "/measureupmenu", to: 'measureup#menu'
+    get "/macros", to: 'measureup#macros'
+    get "/aboutme", to: 'measureup#aboutme'
+  end
 end
